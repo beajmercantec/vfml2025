5800ba3f65c33786a5b9e7f30af674861567bb0b
beajmercantec <beaj@mercantec.dk>
2025-09-12 20:10:11 +0200

    Update Program.cs

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,9 +1,27 @@
-using BlazorUI;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
 
-await builder.Build().RunAsync();
+// Tilføj services
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+// Tilføj dine egne services her
+builder.Services.AddSingleton<DialogManager>();
+
+var app = builder.Build();
+
+// Brug standard opsætning for Blazor Server
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();           // <--- server side SignalR-hub
+app.MapFallbackToPage("/_Host");  // <--- vigtig!
+
+app.Run();
